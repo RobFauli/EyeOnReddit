c0f653900d615bb558e79a8b6f5770a36fed699e
robfauli <robfauli.oss@gmail.com>
2016-08-15 08:24:50 +0200

    Removed Submonitor class, merged functionality into Subreddit class.

--- src/Subreddit.hpp
+++ src/Subreddit.hpp
@@ -21,11 +21,18 @@ public:
     QVector<int> getFrontpageScores() const;
     QList<QUrl> getFrontpageDomains() const;
 
+signals:
+    void postWithHighCommentCount();
+    void postWithHighScore();
+
 private:
     QString m_name;
     QUrl m_url;
     QJsonDocument m_json;
     const QJsonArray getFrontpagePostJsonObjects() const;
+
+    std::shared_ptr<Subreddit> m_sub;
+    std::unique_ptr<QTimer> m_timer;
 };
 
 inline QJsonObject preparepost(const QJsonValue &post)
