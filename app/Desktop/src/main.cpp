208cd3fe02a3acc5bb562c9aa83941bb9caba7f7
robfauli <robfauli.oss@gmail.com>
2016-09-01 08:36:36 +0200

    Reorganizing main.

--- app/Desktop/src/main.cpp
+++ app/Desktop/src/main.cpp
@@ -10,30 +10,30 @@
 
 int main(int argc, char *argv[])
 {
-
-
     QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
     Settings settings("robfauli", "EyeOnReddit");
-
+    
+    // If there is no system tray, stop program immediately
     if (!QSystemTrayIcon::isSystemTrayAvailable()) {
         QMessageBox::critical(0, "Systray",
                                  "Could not detect a system tray on this system.");
         return 1;
     }
-
-
+    
+    // Make types available in QML
     qmlRegisterType<Reddit>("myReddit", 0, 1, "Reddit");
     qmlRegisterUncreatableType<Subreddit>("myReddit", 0, 1, "Subreddit",
                                           "Cannot create Subreddits from QML.");
     qmlRegisterUncreatableType<RedditPost>("myReddit", 0, 1, "RedditPost",
                                            "Cannot create RedditPost from QML.");
-
-    QQmlApplicationEngine engine;
-    // Setup tray
+    
     SystemTrayIcon tray;
     engine.rootContext()->setContextProperty("tray", &tray);
-
     Reddit reddit;
+    engine.rootContext()->setContextProperty("theReddit", &reddit);
+    engine.rootContext()->setContextProperty("settings", &settings);
+
     auto subnames = settings.value("subnames").toList();
     Q_FOREACH(const auto &subname, subnames)
         reddit.addSubreddit(subname.toString());
@@ -51,17 +51,19 @@ int main(int argc, char *argv[])
         }
     );
 
+    // Keep the followed subreddits in settings at all times in case of crash
     QObject::connect(&reddit, &Reddit::subredditNamesChanged,
                      [&reddit, &settings] () {
         settings.setValue("subnames", QVariant(reddit.getSubredditNames()));
         settings.sync();
     });
-    engine.rootContext()->setContextProperty("theReddit", &reddit);
-    engine.rootContext()->setContextProperty("settings", &settings);
+
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
+    
+    // Warn the reddit view in QML about new posts
     auto redditView = engine.rootObjects()[0]->findChild<QObject*>(QStringLiteral("redditView"));
+    qDebug() << redditView;
     QObject::connect(&reddit, SIGNAL(postAlert(QString,QString)), redditView, SIGNAL(postAlert(QString,QString)));
 
     tray.initialize(&engine);
